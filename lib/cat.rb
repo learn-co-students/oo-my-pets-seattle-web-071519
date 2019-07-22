0af8a3441db944a062b569f38b025302f7641f09
Ellyn <eka249@nyu.edu>
2019-07-21 19:25:18 -0700

    commit

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,23 @@
+require_relative '../config/environment.rb'
+
+
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_writer
+  attr_accessor :mood, :owner
+
+  @@all =[]
+
+  def initialize(name, owner)
+    @name=name
+    @owner=owner
+    @mood= "nervous"
+    @@all << self
+    
+  end
+
+  def self.all
+    @@all
+  end
+
 end
